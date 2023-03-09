4078b6e92f1f16991f29f075c7a3050ec220b4dd
GrynnRadovid <126412429+GrynnRadovid@users.noreply.github.com>
2023-03-09 21:17:23 +0200

    Update SWVersion.c

--- EB_tresos/templates/Application_Can/source/stubs/SWVersion.c
+++ EB_tresos/templates/Application_Can/source/stubs/SWVersion.c
@@ -1,6 +1,6 @@
-const uint8 pSWVersionString[23] = "SW VERSION: 01.01.04";
+const uint8 pSWVersionString[23] = "SW VERSION: 01.01.05";
 
 
 const uint8 cu8HWFixVersion = 0x01;
 const uint8 cu8MajorSWVersion = 0x01;
-const uint8 cu8BugFixVersion = 0x04;
+const uint8 cu8BugFixVersion = 0x05;
